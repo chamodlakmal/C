04850061aecf15bf9dfc071e427fb17e8261ae54
chamod_lab <chamodlakmal97@gmail.com>
2020-08-22 18:23:59 +0530

    updated

--- queue.c
+++ queue.c
@@ -2,22 +2,23 @@
 #define MAX 3
 
 int queue[MAX];
-int front=-1,rear=-1;
+int front=-1, rear=-1;
 
 void insert();
 
-void insert(){
+void insert() {
 
     int num;
     printf("Enter the Number :");
-    scanf("%d",&num);
-    if(rear==MAX-1)
+    scanf("%d", &num);
+    if (rear==MAX-1)
         printf("\n OVERFLOW");
-    else if(rear==-1&&front==-1){
-       front=0;
-       rear=0;
-       queue[rear]=num;
-    }else{
+    else if (rear==-1&&front==-1) {
+        front=0;
+        rear=0;
+        queue[rear]=num;
+    }
+    else {
         rear++;
         queue[rear]=num;
     }
