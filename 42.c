57a0865fe16aa38708587d4b92fd38935491e1f8
chamod_lab <chamodlakmal97@gmail.com>
2020-08-22 18:21:11 +0530

    updated

--- 42.c
+++ 42.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-int main(){
+int main() {
     int size;
     printf("Enter your array size :");
-    scanf("%d",&size);
-    if(size>=0){
-       int myArray[size];
-        for(int i=0;i<size;i++){
-            printf("Enter your myArray[%d]th element :",i);
-            scanf("%d",&myArray[i]);
+    scanf("%d", &size);
+    if (size>=0) {
+        int myArray[size];
+        for (int i=0;i<size;i++) {
+            printf("Enter your myArray[%d]th element :", i);
+            scanf("%d", &myArray[i]);
         }
 
         int minimum=myArray[0];
 
-        for(int i=1;i<size;i++){
-            if(minimum>myArray[i]){
-               minimum= myArray[i];
+        for (int i=1;i<size;i++) {
+            if (minimum>myArray[i]) {
+                minimum= myArray[i];
             }
         }
-        printf("Minimum number : %d",minimum);
-    }else{
+        printf("Minimum number : %d", minimum);
+    }
+    else {
         printf("Array size can't be minus value.");
     }
 
